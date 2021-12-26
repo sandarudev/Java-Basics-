a5a6f527478ac4f399624e55116cebd1404c2e89
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2021-12-26 09:06:01 +0530

    Added 20th exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -81,8 +81,35 @@ public class Main {
             System.out.println(foods.get(i));
         }
     **/
-
+    /**
         //20 - 2D ArrayList
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+
+        ArrayList<String> bakeryList = new ArrayList<>();
+        bakeryList.add("Pasta");
+        bakeryList.add("Garlic Bread");
+        bakeryList.add("Donuts");
+
+        ArrayList<String> produceList = new ArrayList<>();
+        produceList.add("Tomatoes");
+        produceList.add("Zuchchini");
+        produceList.add("Peppers");
+
+        ArrayList<String> drinkList = new ArrayList<>();
+        drinkList.add("Soda");
+        drinkList.add("Coffee");
+
+        //Adding sub ArrayLists to the main Array in order to make a 2D Array
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinkList);
+
+        System.out.println(groceryList.get(0).get(1));
+    **/
+
+        //21 - for-each loop
+
+
 
 
 
